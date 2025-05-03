ddafe843b5e8c7125d593e3f5eeb6b51bac6d86f
ahxviey <162808355+ahxviey@users.noreply.github.com>
2025-05-03 23:15:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('it320')); // Serve static files
 
-// MySQL Connection
+const mysql = require('mysql');
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'screentime_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
